89efaa8a7df852b2b7e968af701f980d13593ba6
yangyahang <150992210+yangyahang@users.noreply.github.com>
2024-06-03 15:32:07 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,7 @@
-/* MoveNet Skeleton - Steve's Makerspace (most of this code is from TensorFlow)
-
-MoveNet is developed by TensorFlow:
-https://www.tensorflow.org/hub/tutorials/movenet
-
-*/
-
 let video, bodypose, pose, keypoint, detector;
 let poses = [];
+let t = 0; // 全域變數，用來追蹤插值的進度
+let GIFImg;
 
 async function init() {
   const detectorConfig = {
@@ -48,13 +43,11 @@ function draw() {
   image(video, 0, 0);
   drawSkeleton();
   // flip horizontal
-  cam = get();
+  let cam = get();
   translate(cam.width, 0);
   scale(-1, 1);
   image(cam, 0, 0);
 }
-
-let t = 0; // 全域變數，用來追蹤插值的進度
 
 function drawSkeleton() {
   // 繪製所有追蹤到的關鍵點
@@ -75,11 +68,11 @@ function drawSkeleton() {
     if (partA.score > 0.1 && partB.score > 0.1) {
       push();
       imageMode(CENTER);
-      
+
       // 插值計算位置
       let newX = lerp(partA.x, partB.x, t);
       let newY = lerp(partA.y, partB.y, t);
-      
+
       image(GIFImg, newX, newY, 50, 50);
 
       // 更新插值因子
@@ -100,27 +93,7 @@ function drawSkeleton() {
     }
   }
 }
-function preload(){
-	GIFImg = loadImage("car.gif");
+
+function preload() {
+  GIFImg = loadImage("car.gif");
 }
-
-
-/* Points (view on left of screen = left part - when mirrored)
-  0 nose
-  1 left eye
-  2 right eye
-  3 left ear
-  4 right ear
-  5 left shoulder
-  6 right shoulder
-  7 left elbow
-  8 right elbow
-  9 left wrist
-  10 right wrist
-  11 left hip
-  12 right hip
-  13 left kneee
-  14 right knee
-  15 left foot
-  16 right foot
-*/
